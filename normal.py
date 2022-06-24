c3d703bb4faace4f7f048f06cd3ba0d38c63f2fe
Darshan Patel <darshannayakpura@gmail.com>
2022-06-24 06:25:16 +0530

    added implimentation

--- normal.py
+++ normal.py
@@ -1,19 +1,4 @@
-Python 3.8.2 (tags/v3.8.2:7b3ab59, Feb 25 2020, 22:45:29) [MSC v.1916 32 bit (Intel)] on win32
-Type "help", "copyright", "credits" or "license()" for more information.
->>> for i in range(0, 10);
-SyntaxError: invalid syntax
->>> for i in range(0, 10):
-	print(i)
+print("It's normal file from the the Owner")
 
-	
-0
-1
-2
-3
-4
-5
-6
-7
-8
-9
->>> 
+
+print("Hey, \n This is new change in the Github")
