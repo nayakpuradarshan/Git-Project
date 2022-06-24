70ac86fe9f92794db0abc658a3a6d96a10f38330
Darshan Patel <darshannayakpura@gmail.com>
2022-06-24 05:50:34 +0530

    New file normal from Darshan Patel

---
+++ normal.py
@@ -0,0 +1,19 @@
+Python 3.8.2 (tags/v3.8.2:7b3ab59, Feb 25 2020, 22:45:29) [MSC v.1916 32 bit (Intel)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> for i in range(0, 10);
+SyntaxError: invalid syntax
+>>> for i in range(0, 10):
+	print(i)
+
+	
+0
+1
+2
+3
+4
+5
+6
+7
+8
+9
+>>> 
